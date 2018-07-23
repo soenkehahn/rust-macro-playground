ae26cc9b7408ae7671abb5896ed01a6cdd7897d6
Sönke Hahn <soenkehahn@gmail.com>
2018-07-23 00:42:10 -0400

    parse applications as flat lists

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,26 @@
 #[derive(Debug)]
 enum Ast {
-    Lambda {
-        parameter: String,
-        body: Box<Ast>,
-    },
-    Var {
-        variable: String,
-    },
-    App {
-        function: Box<Ast>,
-        argument: Box<Ast>,
-    },
+    Lambda { parameter: String, body: Box<Ast> },
+    Var { variable: String },
+    AppChain { terms: Vec<Ast> },
 }
 
 fn pp(ast: Ast) -> String {
     match ast {
         Ast::Var { variable } => variable,
         Ast::Lambda { parameter, body } => format!("|{}| {}", parameter, pp(*body)),
-        Ast::App { function, argument } => format!("({})({})", pp(*function), pp(*argument)),
+        Ast::AppChain { terms } => {
+            let mut result = terms
+                .into_iter()
+                .map(|term| format!("({})", pp(term)))
+                .collect::<Vec<String>>();
+            result.reverse();
+            result.join(" ")
+        }
     }
 }
 
 macro_rules! ast {
-    (($($x:tt)+)) => {
-        ast!($($x)+)
-    };
-    (@ $f:tt $x:tt) => {
-        Ast::App {
-            function: Box::new(ast!($f)),
-            argument: Box::new(ast!($x)),
-        }
-    };
     (| $parameter:ident | $body:tt) => {
         Ast::Lambda {
             parameter: stringify!($parameter).to_string(),
@@ -42,10 +32,29 @@ macro_rules! ast {
             variable: stringify!($x).to_string(),
         }
     };
+    (( $($t:tt)+ )) => {
+        ast!($($t)+)
+    };
+    ($($t:tt)+) => {
+        ast_h!([ ] $($t)+)
+    };
+}
+
+macro_rules! ast_h {
+    ([ $($stack:tt),* ] $token:tt $($rest:tt)*) => {
+        ast_h!([ $token $(, $stack)* ] $($rest)*)
+    };
+    ([ $($stack:tt),* ]) => {
+        {
+            Ast::AppChain{
+                terms: vec![$(ast!($stack)),*]
+            }
+        }
+    };
 }
 
 fn main() {
-    let term = ast!(y);
+    let term = ast!(a b c);
     println!("{:?}", term);
 }
 
@@ -69,7 +78,7 @@ mod test {
 
     #[test]
     fn parses_applications() {
-        assert_eq!(pp(ast!(@ x (y))), "(x)(y)");
+        assert_eq!(pp(ast!(x y)), "(x) (y)");
     }
 
     #[test]
@@ -79,11 +88,11 @@ mod test {
 
     #[test]
     fn parses_complex_terms() {
-        assert_eq!(pp(ast!(@ (|x| x) (y))), "(|x| x)(y)");
+        assert_eq!(pp(ast!((|x| x)(y))), "(|x| x) (y)");
     }
 
     #[test]
     fn parses_chains_of_applications() {
-        assert_eq!(pp(ast!(@ (@ a b) c)), "((a)(b))(c)");
+        assert_eq!(pp(ast!(a b c)), "(a) (b) (c)");
     }
 }
